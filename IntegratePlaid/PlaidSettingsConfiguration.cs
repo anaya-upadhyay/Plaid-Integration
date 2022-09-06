11822841379b07274fbd40910d13a282d9377fe0
Anaya Upadhyay <anaya.upadhyay@amniltech.com>
2022-09-06 14:22:51 +0545

    Add newer configs

--- IntegratePlaid/PlaidSettingsConfiguration.cs
+++ IntegratePlaid/PlaidSettingsConfiguration.cs
@@ -2,12 +2,17 @@
 {
     public class PlaidSettingsConfiguration
     {
+        public string Environment { get; set; }
+
         public string LinkTokenURL { get; set; }
         public string PublicTokenURL { get; set; }
         public string AccessTokenURL { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string PublicKey { get; set; }
 
+        public string DevTokenURL { get; set; }
+        public string DevClientId { get; set; }
+        public string DevSecret { get; set; }
     }
+
 }
